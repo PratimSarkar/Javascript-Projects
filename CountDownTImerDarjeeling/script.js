4ce83dc7894e3e097e0a3ce3822334d4477329cd
Pratim Sarkar <pratimsarkar003@gmail.com>
2021-09-14 02:08:08 +0530

    Dynamic BG added (#1)
    Co-authored-by: Pratim Sarkar <pratimsarkar@Pratims-MacBook-Air.local>

--- CountDownTImerDarjeeling/script.js
+++ CountDownTImerDarjeeling/script.js
@@ -5,6 +5,22 @@ const daysel = document.getElementById('days');
 const hoursel = document.getElementById('hours');
 const minsel = document.getElementById('mins');
 const secondsel = document.getElementById('seconds');
+let curImg = 1;
+let bgImgs = document.querySelectorAll(".bg-imgs img");
+    setInterval(() => {
+        /* if first image (curImg = 0), then unload last image */
+        let prevImg = curImg? curImg-1: bgImgs.length-1;
+
+        bgImgs[prevImg].style.opacity = 0;
+        bgImgs[curImg].style.opacity = 100;
+        curImg++;
+
+        /* if last image, go back to first image */
+        if (curImg >= bgImgs.length) {
+            curImg = 0;
+        }
+    / *every 10 seconds */
+}, 5000);
 function countdown(){
     const currentdate = new Date();
     const matchdate= new Date(matchday);
